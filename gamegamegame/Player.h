8ff083ab06bebd200f7c8b362b5fe5625c4477fe
Daniel Calle <daniel@calle.org>
2023-11-30 00:45:15 -0800

    Map finished, might want adjustments still, colors updated for a better look, character changed slightly

--- gamegamegame/Player.h
+++ gamegamegame/Player.h
@@ -2,18 +2,18 @@
 
 #include "SFML/Graphics.hpp"
 
-class Player : public sf::CircleShape
+class Player : public sf::RectangleShape
 {
 public:
 	Player(const sf::Vector2f& pos, const float& radius) :
-		sf::CircleShape(radius)
+		sf::RectangleShape(pos)
 	{
 		this->setPosition(pos);
 	}
 
 	void setTexture(const sf::Texture* texture)
 	{
-		sf::CircleShape::setTexture(texture);
+		sf::RectangleShape::setTexture(texture);
 	}
 
 private:
